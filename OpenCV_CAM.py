498841c5e2c21b7b627a9e112da1ca8be1d2cab9
Benoit <bensuperpc@gmail.com>
2020-05-28 21:53:19 +0000

    Remove old code

--- OpenCV_CAM.py
+++ OpenCV_CAM.py
@@ -6,18 +6,11 @@ print(os.environ.get('CAMERA'))
 cap = cv2.VideoCapture(os.environ.get('CAMERA'))
 
 while(True):
-    # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    gray = frame
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame', gray)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
